5bcd20ca71d13889fe73525b45e45feaaf645f98
Camille Rey <camille.celine.rey@gmail.com>
2021-03-10 14:54:50 +0100

    Add username to Users model and validations in recipes model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :intructions
-  has_many :recipe_tags
-  has_many :ingredients
+  has_many :intructions, dependent: :destroy
+  has_many :recipe_tags, dependent: :destroy
+  has_many :ingredients, dependent: :destroy
   has_many :users, through: :saved_recipes
 end
