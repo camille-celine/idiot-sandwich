14ca5b8cb821c07363a32043e937c91c5245e58b
Camille Rey <camille.celine.rey@gmail.com>
2021-03-10 15:01:23 +0100

    Correcting typo in recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :intructions, dependent: :destroy
+  has_many :instructions, dependent: :destroy
   has_many :recipe_tags, dependent: :destroy
   has_many :ingredients, dependent: :destroy
   has_many :users, through: :saved_recipes
