20258b98a2c4d2647aa83ca4fa5ce4dc87539fae
Camille Rey <camille.celine.rey@gmail.com>
2021-03-26 15:55:33 +0100

    IS-7 instructions field to recipe + ingredients and measurement seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_134550) do
+ActiveRecord::Schema.define(version: 2021_03_26_091747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_134550) do
 
   create_table "instructions", force: :cascade do |t|
     t.integer "step_number"
-    t.text "intruction_text"
+    t.text "instruction_text"
     t.bigint "recipe_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Deleting measurement types and ingredients'
+MeasurementLookup.destroy_all
+IngredientList.destroy_all
+
+puts 'Creating 10 measurement types...'
+10.times do 
+    measurement_type = MeasurementLookup.new(
+        measurement_type: Faker::Food.metric_measurement
+    )
+    measurement_type.save!
+end
+puts 'Created!'
+
+puts 'Creating 100 ingredients...'
+100.times do
+    ingredient = IngredientList.new(
+        name: Faker::Food.ingredient 
+    )
+    ingredient.save!
+end
+puts 'Created!'
